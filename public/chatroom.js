998128bb39c662a55c0ffff1ed3180afff6bebdb
betweak12 <ys@betweak.com>
2021-07-28 16:26:29 +0900

    Make chatroom

--- public/chatroom.js
+++ public/chatroom.js
@@ -1,14 +1,29 @@
 // io(namespace);
+const username = prompt('닉네임 입력');
 
 const socket = io('/CHATROOM');
 socket.on('connect', function () {
   console.log('connect');
+
+  alert(`${username}님이 로그인 하셨습니다.`);
+});
+socket.emit('join-room', roomId, username);
+
+socket.on('msg:broad', (msg) => {
+  console.log(msg);
+  createMessagebox(`공지: ${msg}`);
+});
+socket.on('msg:get', (msg) => {
+  console.log(msg);
+  createMessagebox(msg);
 });
 
 function sendMessage() {
   const input = document.querySelector('input#msg');
-  console.log(input.value);
+  const data = input.value;
   input.value = '';
+  socket.emit('msg:send', data);
+  createMessagebox(`${username}: ${data}`);
 }
 
 function createMessagebox(msg) {
--- socketio.js
+++ socketio.js
@@ -19,25 +19,30 @@ module.exports = (io) => {
 
   const chatRoom = io.of('/CHATROOM');
   chatRoom.on('connection', (socket) => {
-    socket.on('join-room', (roomId) => {
+    socket.on('join-room', (roomId, username) => {
       socket.join(roomId);
       console.log(roomId);
+      // 환영메시지
+      chatRoom.to(roomId).emit('msg:broad', `${username}님이 입장하였습니다.`);
+
       socket.on('leave-room', (roomId) => {
         socket.leave(roomId);
       });
-
-      socket.on('message', (msg) => {
+      socket.on('msg:send', (msg) => {
         console.log(msg);
-        socket.to(roomId).emit('message', `socket ${msg}`);
-        chatRoom.to(roomId).emit('message', `ns: ${msg}`);
-
-        // NameSpace vs socket의 차이:
-        // 메시지를 전송하는 주체가 관리자 level
-        // vs
-        // 소켓 level
-        chatRoom.emit('message');
-        // Room상관없이 이벤트를 발생
+        socket.to(roomId).emit('msg:get', msg);
       });
+      // socket.on('message', (msg) => {
+      //   socket.to(roomId).emit('message', `socket ${msg}`);
+      //   chatRoom.to(roomId).emit('message', `ns: ${msg}`);
+
+      //   // NameSpace vs socket의 차이:
+      //   // 메시지를 전송하는 주체가 Namespace level
+      //   // chatRoom.to()...
+      //   // vs
+      //   // 소켓 level (socket.to()...)
+      //   // Room상관없이 이벤트를 발생
+      // });
     });
   });
 };
